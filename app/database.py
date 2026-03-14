6b80a2bf86b8b54c42aae4a21f5f3787865aa146
maxxzxv <maksoonfs@gmail.com>
2026-03-14 12:10:25 +0100

    Implemented resources monitoring for a machine

--- app/database.py
+++ app/database.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# database url format "postgresql://username:password@hostname:port/database"
+# change depending on your needs
 DATABASE_URL = "postgresql://monitoring_user:mntusr-pass-3517@localhost:5432/monitoring_db"
 
 engine = create_engine(DATABASE_URL)
